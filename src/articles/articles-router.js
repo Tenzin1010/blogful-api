f5857d05530bec6b3685070025de05da39c3f853
Tenzin Ngawang <tenzin.ngawang0@gmail.com>
2019-10-03 14:49:57 -0400

    Restful API section

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -8,65 +8,86 @@ const ArticlesService = require('./articles-service')
 const articlesRouter = express.Router()
 const jsonParser = express.json()
 
-articlesRouter
-  .route('/')
-  .get((req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getAllArticles(knexInstance)
-      .then(articles => {
-        res.json(articles)
-      })
-      .catch(next)
+  const serializeArticle = article => ({
+    id: article.id,
+    style: article.style,
+    title: xss(article.title),
+    content: xss(article.content),
+    date_published: article.date_published,
   })
-  .post(jsonParser, (req, res, next) => {
-    const { title, content, style } = req.body
-    const newArticle = { title, content, style }
+  
+  articlesRouter
+    .route('/')
+    .get((req, res, next) => {
+      const knexInstance = req.app.get('db')
+      ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+          res.json(articles.map(serializeArticle))
+        })
+        .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+      const { title, content, style } = req.body
+      const newArticle = { title, content, style }
 
-    if(!title) {
-      return res.status(400).json({
-        error: {message: `Missing 'title' in request body`}
-      })
-    }
-    if(!content) {
-      return res.status(400).json({
-        error: {message: `Missing 'content' in request body`}
-      })
-    }
+      //if(!title) & if(!content) can be replaced using ` for (const [key, value]'
+      // if(!title) {
+      //   return res.status(400).json({
+      //     error: {message: `Missing 'title' in request body`}
+      //   })
+      // }
+      // if(!content) {
+      //   return res.status(400).json({
+      //     error: {message: `Missing 'content' in request body`}
+      //   })
+      // }
+      for (const [key, value] of Object.entries(newArticle))
+        if (value == null)
+          return res.status(400).json({
+            error: { message: `Missing '${key}' in request body` }
+          })
+  
+      ArticlesService.insertArticle(
+        req.app.get('db'),
+        newArticle
+      )
+        .then(article => {
+          res
+            .status(201)
+            .location(`/articles/${article.id}`)
+            .json(serializeArticle(article))
+        })
+        .catch(next)
+    })
 
-    //FOR can replace the IF(!key) since its repetitive  
-    // for (const [key, value] of Object.entries(newArticle)) {
-    //   if (value == null) {
-    //     return res.status(400).json({
-    //       error: { message: `Missing '${key}' in request body` }
-    //     })
-    //   }
-    // }
-
-    ArticlesService.insertArticle(
-      req.app.get('db'),
-      newArticle
-    )
-      .then(article => {
-        res
-          .status(201)
-          .location(`/articles/${article.id}`)
-          .json(article)
-      })
-      .catch(next)
-  })
-
-articlesRouter
+  articlesRouter
   .route('/:article_id')
-  .get((req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getById(knexInstance, req.params.article_id)
+  .all((req, res, next) => {
+    ArticlesService.getById(
+      req.app.get('db'),
+      req.params.article_id
+    )
       .then(article => {
         if (!article) {
           return res.status(404).json({
             error: { message: `Article doesn't exist` }
           })
         }
-        res.json(article)
+        res.article = article
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeArticle(res.article))
+  })
+  .delete((req, res, next) => {
+    ArticlesService.deleteArticle(
+      req.app.get('db'),
+      req.params.article_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
       })
       .catch(next)
   })
--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -2,7 +2,7 @@
 //the test will check to see if the results hold true compared to the article router results
 const knex = require('knex')
 const app = require('../src/app')
-const { makeArticlesArray } = require('./articles.fixtures')
+const { makeArticlesArray } = require('./api/articles.fixtures')
 
 describe('Articles Endpoints', function() {
   let db
@@ -21,11 +21,14 @@ describe('Articles Endpoints', function() {
 
   afterEach('cleanup',() => db('blogful_articles').truncate())
 
-  describe(`GET /articles`, () => {
+  //GET /api/articles TEST
+  describe(`GET /api/articles`, () => {
     context(`Given no articles`, () => {
+
+//><<<<<<<<<<<<<<<<<PASSED
       it(`responds with 200 and an empty list`, () => {
         return supertest(app)
-          .get('/articles')
+          .get('/api/articles')
           .expect(200, [])
       })
     })
@@ -41,18 +44,19 @@ describe('Articles Endpoints', function() {
 //<<<<<<<<<<<<<<<<<<BUG HERE >>>>>>>>>>>>>>>
       it('responds with 200 and all of the articles', () => {
         return supertest(app)
-          .get('/articles')
+          .get('/api/articles')
           .expect(200, testArticles)
       })
     })
   })
 
-  describe(`GET /articles/:article_id`, () => {
+  //GET /:article_id TEST
+  describe(`GET /api/articles/:article_id`, () => {
     context(`Given no articles`, () => {
       it(`responds with 404`, () => {
         const articleId = 123456
         return supertest(app)
-          .get(`/articles/${articleId}`)
+          .get(`/api/articles/${articleId}`)
           .expect(404, { error: { message: `Article doesn't exist` } })
       })
     })
@@ -70,14 +74,15 @@ describe('Articles Endpoints', function() {
         const articleId = 2
         const expectedArticle = testArticles[articleId - 1]//?????
         return supertest(app)
-          .get(`/articles/${articleId}`)
+          .get(`/api/articles/${articleId}`)
           .expect(200, expectedArticle)
       })
     })
   })
 
-//PASSED
-  describe(`POST /articles`, () => {
+// POST TEST 
+  describe(`POST /api/articles`, () => {
+//>>>>>>>>>>>>BUG HERE>>>>>>>>>>>>>>>>>
     it(`creates an article, responding with 201 and the new article`, function() {
       this.retries(3)
       const newArticle = {
@@ -86,7 +91,7 @@ describe('Articles Endpoints', function() {
         content: 'Test new article content...'
       }
       return supertest(app)
-        .post('/articles')
+        .post('/api/articles')
         .send(newArticle)
         .expect(201)
         .expect(res => {
@@ -94,20 +99,20 @@ describe('Articles Endpoints', function() {
           expect(res.body.style).to.eql(newArticle.style)
           expect(res.body.content).to.eql(newArticle.content)
           expect(res.body).to.have.property('id')
-          expect(res.headers.location).to.eql(`/articles/${res.body.id}`)
+          expect(res.headers.location).to.eql(`/api/articles/${res.body.id}`)
           const expected = new Date().toLocaleString('en', { timeZone: 'UTC' })
           const actual = new Date(res.body.date_published).toLocaleString()
           expect(actual).to.eql(expected)
         })
         .then(res =>
           supertest(app)
-            .get(`/articles/${res.body.id}`)
+            .get(`/api/articles/${res.body.id}`)
             .expect(res.body)
         )
     })
     it(`responds with 400 and a Error Message when the 'title' is missing`, () => {
       return supertest(app)
-        .post('/articles')
+        .post('/api/articles')
         .send({
           style: 'Listicle',
           content: 'Test new article content'
@@ -118,7 +123,7 @@ describe('Articles Endpoints', function() {
     })
     it(`responds with 400 and an error message when the 'content' is missing`, () => {
       return supertest(app)
-        .post('/articles')
+        .post('/api/articles')
         .send({
           title: 'Test new article',
           style: 'Listicle',
@@ -129,7 +134,7 @@ describe('Articles Endpoints', function() {
     })
     it(`responds with 400 and an error message when the 'style' is missing`, () => {
       return supertest(app)
-        .post('/articles')
+        .post('/api/articles')
         .send({
           title: 'Test new article',
           content: 'Test new article content...'
@@ -139,7 +144,7 @@ describe('Articles Endpoints', function() {
         })
     })
     
-    //this can replace the 400 (missing BODY parts)
+    //>>>REFACTORED CODE __this can replace the 400 (missing BODY parts, for title, content or style)
     // const requiredFields = ['title', 'style', 'content']
     // requiredFields.forEach(field => {
     //  const newArticle = {
@@ -152,139 +157,47 @@ describe('Articles Endpoints', function() {
     //    delete newArticle[field]
 
     //    return supertest(app)
-    //      .post('/articles')
+    //      .post('/api/articles')
     //      .send(newArticle)
     //      .expect(400, {
     //        error: { message: `Missing '${field}' in request body` }
     //      })
     //  })
     // })
+  })
+
+  //DELETE TEST
+  describe(`DELETE /api/articles/:article_id`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 404`, () => {
+        const articleId = 123456
+        return supertest(app)
+          .delete(`/api/articles/${articleId}`)
+          .expect(404, { error: { message: `Article doesn't exist` } })
+      })
+    })
+
+    context('Given there are articles in the database', () => {
+      const testArticles = makeArticlesArray()
+
+      beforeEach('insert articles', () => {
+        return db
+          .into('blogful_articles')
+          .insert(testArticles)
+      })
+      //>>>>>>>BUG>>>>>>>>>>>>>>>
+      it('responds with 204 and removes the article', () => {
+        const idToRemove = 2
+        const expectedArticles = testArticles.filter(article => article.id !== idToRemove)
+        return supertest(app)
+          .delete(`/api/articles/${idToRemove}`)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get(`/api/articles`)
+              .expect(expectedArticles)
+          )
+      })
+    })
+  })
 })
-
-// const { expect } = require('chai')
-// const knex = require('knex')
-// const app = require('../src/app')
-// const { makeArticlesArray } = require('./articles.fixtures')
-
-// describe.only('Articles Endpoint', function () {
-//     let db
-
-//     //creating the knex instance, executed before any describe
-//     before('make Knex instance', () => {
-//         db = knex({
-//             client: 'pg',
-//             connection: process.env.TEST_DB_URL,
-//         })
-//         app.set('db', db)
-//     })
-
-//     //
-//     //executed after all the describe has completed
-//     after('disconnect from db', () => db.destroy())
-
-//     //
-//     before('clean the table', () => db('blogful_articles').truncate())
-
-//     // removes any table data so that the next test has a clean start.
-//     afterEach('cleanup', () => db('blogful_articles').truncate())
-
-//     //testing GET endpoint /articles 
-//     describe(`GET /articles`, () => {
-
-//         //testing /articles with NO DATA
-//         context(`Given no articles`, () => {
-//             it(`responds with 200 and an empty list`, () => {
-//             return supertest(app)
-//                 .get('/articles')
-//                 .expect(200, [])
-//             })
-//         })
-//         //testing /articles DATA present
-//         context('Given there are articles in the database', () => {
-//           const testArticles = makeArticlesArray()
-//         //insert data in to table
-//         beforeEach('insert articles', () => {
-//         return db
-//             .into('blogful_articles')
-//             .insert(testArticles)
-//         })
-    
-//           it('responds with 200 and all of the articles', () => {
-//             return supertest(app)
-//               .get('/articles')
-//               .expect(200, testArticles)
-//           })
-//         })
-//       })
-
-//     //testing GET endpoint /articles/:article_id
-//     describe(`Get /articles/:article_id`, () => {
-
-//         //testing with NO DATA
-//         context(`Given no articles`, () => {
-//             it(`responds with 404`, () => {
-//             const articleId = 123456
-//             return supertest(app)
-//                 .get(`/articles/${articleId}`)
-//                 .expect(404, { error: { message: `Article doesn't exist` } })
-//             })
-//         })
-
-//         //testing with DATA
-//         context('Given there are articles in the DataBase', () => {
-//             const testArticles = makeArticlesArray()
-
-//             //populate the table with test data
-//             beforeEach('insert articles', () => {
-//                 return db
-//                     .into('blogful_articles')
-//                     .insert(testArticles)
-//             })
-
-//             it('GET /article/:article_id responds with 200 and the specified articles', () => {
-//                 //whats going on here?????
-//                 const articleId = 2
-//                 const expectedArticle = testArticles[articleId - 1]
-//                 return supertest(app)
-//                     .get('./articles/${articleId}')
-//                     .expect(200, expectedArticle)
-//             })
-//         })
-
-//     })
-
-//     //testing POST endpoint /articles
-//     //how does creating a endpoing Post /articles in app.js make the test work????????
-//     describe.only(`Post /articles`, () => {
-//         it(`creates an article, responding with 201 and the new article`, function() {
-//             this.retries(3)
-            
-//             const newArticle = {
-//                 title: 'Test new Article',
-//                 style: 'Listicle',
-//                 content: 'Test new article content'
-//             }
-//             return supertest(app)
-//             .post('/articles')
-//             .send(newArticle)
-//             .expect(201)
-//             .expect( res => {
-//                 expect(res.body.title).to.eql.apply(newArticle.title)
-//                 expect(res.body.style).to.eql.apply(newArticle.style)
-//                 expect(res.body.content).to.eql.apply(newArticle.content)
-//                 expect(res.body).to.have.property('id')
-//                 expect(res.headers.location).to.eql(`/articles/${res.body.id}`)
-//                 const expected = new Date().toLocaleString('en', { timeZone: 'UTC' })
-//                 const actual = new Date(res.body.date_published).toLocaleString()
-//                 expect(actual).to.eql(expected)
-//             })
-//             })
-//             .then(postRes =>
-//                 supertest(app)
-//                     .get(`/articles/${postRes.body.id}`)
-//                     .expect(postRes.body)
-//                     )
-//                 })
-//               })
-//             })
-
